a3836cfc1435eeff963615678818e2db78132d98
jusung.kim <wntjd200@naver.com>
2023-07-21 14:30:35 +0900

    완료

---
+++ buildSrc/build/generated-sources/kotlin-dsl-plugins/kotlin/PrecompiledPlugin.kt
@@ -0,0 +1,17 @@
+/**
+ * Precompiled [precompiled.gradle.kts][Precompiled_gradle] script plugin.
+ *
+ * @see Precompiled_gradle
+ */
+class PrecompiledPlugin : org.gradle.api.Plugin<org.gradle.api.Project> {
+    override fun apply(target: org.gradle.api.Project) {
+        try {
+            Class
+                .forName("Precompiled_gradle")
+                .getDeclaredConstructor(org.gradle.api.Project::class.java, org.gradle.api.Project::class.java)
+                .newInstance(target, target)
+        } catch (e: java.lang.reflect.InvocationTargetException) {
+            throw e.targetException
+        }
+    }
+}
